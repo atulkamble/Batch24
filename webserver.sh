9a96e2affe4f09e595621db0b5c50ee6965cab8f
Atul Kamble <atul_kamble@hotmail.com>
2026-07-30 12:14:28 +0530

    Create webserver.sh

---
+++ webserver.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "installing apache"
+sudo apt update
+sudo apt install apache2 -y 
+echo "starting apache service"
+sudo systemctl start apache2
+sudo systemctl enable apache2 
+cd /var/www/html 
+sudo chmod 755 /var/www/html 
+sudo rm index.html
+sudo touch index.html 
+echo "<h1>Webserver</h1>" | sudo tee /var/www/html/index.html > /dev/null
+echo "Done!"
